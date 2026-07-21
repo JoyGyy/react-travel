1d671ceb9abce4d25f33d26ac76011bcece1869d
joygy <joygyzhi@outlook.com>
2026-07-21 11:30:00 +0800

    style: 修复 antfu 中 if 语句未换行问题

--- src/api/attractions.ts
+++ src/api/attractions.ts
@@ -4,12 +4,24 @@ import { request } from './client'
 
 function buildQuery(filters: AttractionFilters = {}) {
   const params = new URLSearchParams()
-  if (filters.city) params.set('city', filters.city)
-  if (filters.ticketType) params.set('ticketType', filters.ticketType)
-  if (filters.keyword) params.set('keyword', filters.keyword)
-  if (filters.tag) params.set('tag', filters.tag)
-  if (filters.page) params.set('page', String(filters.page))
-  if (filters.pageSize) params.set('pageSize', String(filters.pageSize))
+  if (filters.city) {
+    params.set('city', filters.city)
+  }
+  if (filters.ticketType) {
+    params.set('ticketType', filters.ticketType)
+  }
+  if (filters.keyword) {
+    params.set('keyword', filters.keyword)
+  }
+  if (filters.tag) {
+    params.set('tag', filters.tag)
+  }
+  if (filters.page) {
+    params.set('page', String(filters.page))
+  }
+  if (filters.pageSize) {
+    params.set('pageSize', String(filters.pageSize))
+  }
   const query = params.toString()
   return query ? `?${query}` : ''
 }
